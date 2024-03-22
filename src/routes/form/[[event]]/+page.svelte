e0e421cb82b754aa18a5ef28c851056d0b70c4d2
nickydonna <nicky@sudolabs.co>
2024-03-22 15:05:34 -0300

    Improve creationg UI

--- src/routes/form/[[event]]/+page.svelte
+++ src/routes/form/[[event]]/+page.svelte
@@ -8,16 +8,17 @@
 	import { parseTaskText, unparseTaskText } from '$lib/parser';
 
 	import * as pkg from 'rrule';
-	import { Helper } from 'flowbite-svelte';
+	import { Accordion, AccordionItem, Badge, Helper } from 'flowbite-svelte';
 	// @ts-expect-error - see https://github.com/jkbrzt/rrule/issues/548
-	const { RRule } =  pkg.default || pkg;
-	
+	const { RRule } = pkg.default || pkg;
+
 	/** @type {import('./$types').PageData}*/
 	export let data;
-		/** @type {import('./$types').ActionData} */
+	/** @type {import('./$types').ActionData} */
 	export let form;
 
-	let taskText = form?.originalText ?? (data.event ? unparseTaskText(data.event) : ''); // Duplicate to avoid chainging the prop
+	const originalText = form?.originalText ?? (data.event ? unparseTaskText(data.event) : ''); // Duplicate to avoid chainging the prop
+	let taskText = originalText;
 	const today = new Date();
 	/** @type {boolean} */
 	let editting;
@@ -25,14 +26,14 @@
 	let taskInfo;
 	$: {
 		taskInfo = parseTaskText(taskText, today);
-		editting = typeof data.event !== 'undefined'
+		editting = typeof data.event !== 'undefined';
 	}
 
-	// /** @type {import('./$types').Snapshot<string>} */
-	// export const snapshot = {
-	// 	capture: () => taskText,
-	// 	restore: (value) => (taskText = value)
-	// };
+	/** @type {import('./$types').Snapshot<string>} */
+	export const snapshot = {
+		capture: () => taskText,
+		restore: (value) => (taskText = value)
+	};
 
 	// TODO use current date?
 	let dateStr = formatISO(today);
@@ -41,13 +42,51 @@
 <div class="flex h-full w-full flex-row items-center align-middle">
 	<div class="flex-1 p-10">
 		<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-			{ editting ? 'Edit Event' : 'New Event' }
+			{editting ? 'Edit Event' : 'New Event'}
 		</h5>
-		<p>
-			For creating tasks we use text rather than controls. You can see the details of each on the
-			table below
-		</p>
-		<p>Hover over each line to see details</p>
+		<p>For creating tasks we use text rather than controls.</p>
+		<Accordion flush>
+			<AccordionItem>
+				<span slot="header">Explanation</span>
+				<p class="mb-2 text-gray-600 dark:text-gray-400">
+					<span class="mr-1 font-semibold">Title:</span>
+					The title is what remains after all other modifers have been parsed.
+				</p>
+				<p class="mb-2 text-gray-600 dark:text-gray-400">
+					<span class="mr-1 font-semibold">Type:</span>To type the task, prefix with <code>@</code>, can be <code>Block</code>,
+					<code>Event</code>, <code>Task</code> or <code>Remainder</code>
+				</p>
+				<p class="mb-2 text-gray-600 dark:text-gray-400">
+				<span class="mr-1 font-semibold">Status:</span>
+					The status of task can be indicated by prefixing with <code>%</code>, can be <code>back</code>,
+					<code>todo</code>, <code>doing</code> or <code>done</code>
+				<p class="mb-2 text-gray-600 dark:text-gray-400">
+					<span class="mr-1 font-semibold">Dates:</span>To set dates of a task/event. It will be parsed from the text in parenthesis. After the
+					date you can add a <code>|</code> and a recurrence pattern (check rrule.js).
+				</p>
+				<p class="mb-2 text-gray-600 dark:text-gray-400">
+					Example: Go shopping (at 21 until 23 | every monday) -> Date: today 9pm until 23:00 [every week on Monday]
+				</p>
+				<p class="mb-2 text-gray-600 dark:text-gray-400">
+					<span class="mr-1 font-semibold">Tags:</span>To set tags just add <code>#</code> before a word.
+				</p>
+				<p class="mb-2 text-gray-600 dark:text-gray-400">
+					<span class="mr-1 font-semibold">Importance:</span>
+					To set the importance of the task use <code>?</code> for less import or <code>!</code>
+					for more important ones. You can add up to 3 of either. <code>??</code> means
+					-2, and <code>!</code> means +1
+				</p>
+				<p class="mb-2 text-gray-600 dark:text-gray-400">
+					<span class="mr-1 font-semibold">Urgency:</span>
+					Urgency is dictated by <code>^</code>. You can add up to 3.
+				</p>
+				<p class="mb-2 text-gray-600 dark:text-gray-400">
+					<span class="mr-1 font-semibold">Load:</span>
+					Load is dictated by <code>^</code>. You can add up to 3.
+				</p>
+			</AccordionItem>
+		</Accordion>
+
 		<div>
 			<form method="POST" class="mt-2 flex-1">
 				<FloatingLabelInput
@@ -62,63 +101,112 @@
 					Type your task
 				</FloatingLabelInput>
 				{#if form?.errors}
-				<Helper class="pt-2 text-red-950">
-					Please fix the following errors
-					<ul class="list-disc">
-						{#each form.errors as e}
-							<li>{e}</li>
-						{/each}
-					</ul>
-				</Helper>
+					<Helper class="pt-2 text-red-950">
+						Please fix the following errors
+						<ul class="list-disc">
+							{#each form.errors as e}
+								<li>{e}</li>
+							{/each}
+						</ul>
+					</Helper>
 				{/if}
 				<div class="mt-3 flex">
 					<div class="flex-1">
-						<dl class="divide-y divide-gray-100">
-							<div class="px-4 py-2 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0" id="title-info">
-								<dt class="text-sm font-medium leading-6 text-gray-900">Title</dt>
-								<dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-									{taskInfo.title}
-									<Popover placement="bottom" triggeredBy="#title-info" title="The title of the task">
-										The title of the task. It will what remains after parsing all modifiers.
-									</Popover>
-								</dd>
+						<div class="mb-1 flex text-lg">
+							<p class="mr-1 font-semibold">Title:</p>
+							<p>{taskInfo.title}</p>
+						</div>
+						{#if taskInfo.tag.length > 0}
+							<div class="mb-1 flex">
+								<p class="mr-1 font-semibold">Tags: {' '}</p>
+								{#each taskInfo.tag as t (t)}
+									<Badge rounded class="mr-1" color="purple">{t}</Badge>
+								{/each}
 							</div>
-							<div class="px-4 py-2 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0" id="type-info">
-								<dt class="text-sm font-medium leading-6 text-gray-900">
-									Type (@)
-									<Popover placement="bottom" triggeredBy="#type-info" title="Type of the task">
-										The type of task, prefix with <code>@</code>, can be <code>Block</code>,
-										<code>Event</code>, <code>Task</code> or <code>Remainder</code>
-									</Popover>
-								</dt>
-								<dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-									{taskInfo.type}
-								</dd>
+						{/if}
+						<div class="mt-3 flex">
+							<div class="flex-0 px-1">
+								<div class="mb-1 border-b border-solid border-green-500">Type</div>
+								<div>
+									@{taskInfo.type}
+								</div>
 							</div>
-							<div class="px-4 py-2 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0" id="date-info">
-								<dt class="text-sm font-medium leading-6 text-gray-900">Date (.*)</dt>
-								<dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-									{#if taskInfo.date}
-										{format('dd MMM yyyy HH:mm', taskInfo.date)}
-										{#if taskInfo.endDate}
-											-
-											{#if !isSameDay(taskInfo.endDate, taskInfo.date)}
-												{format('dd MMM yyyy HH:mm', taskInfo.endDate)}
-											{:else}
-												{format('HH:mm', taskInfo.endDate)}
-											{/if}
+							<div class="flex-0 px-1">
+								<div class="mb-1 border-b border-solid border-gray-400">Status</div>
+								<div>
+									%{taskInfo.status}
+								</div>
+							</div>
+							{#if taskInfo.date}
+								<div class="flex-0 px-1">
+									<div class="mb-1 border-b border-solid border-gray-400">From</div>
+									<div>
+										{format('dd/MM/yy HH:mm', taskInfo.date)}
+									</div>
+								</div>
+							{/if}
+							{#if taskInfo.endDate}
+								<div class="flex-0 px-1">
+									<div class="mb-1 border-b border-solid border-gray-400">Until</div>
+									<div>
+										{#if taskInfo.date && !isSameDay(taskInfo.endDate, taskInfo.date)}
+											{format('dd/MM/yy HH:mm', taskInfo.endDate)}
+										{:else}
+											{format('HH:mm', taskInfo.endDate)}
 										{/if}
-									{/if}
-									{#if taskInfo.recur}
-									  <p>| {RRule.fromString(taskInfo.recur).toText()}</p>
-									{/if}
-									<Popover placement="bottom" triggeredBy="#date-info" title="The date of the task">
-										The date of this task/event. It will be parsed from the text in parenthesis. 
-										After the date you can add a <code>|</code> and a recurrence pattern (check rrule.js)
-										Example: Go shopping (at 21 until 23 | every monday) -> Date: today 9pm until 23:00 [every week on Monday]
-									</Popover>
-								</dd>
-							</div>
+									</div>
+								</div>
+							{/if}
+							{#if taskInfo.recur}
+								<div class="flex-0 px-1">
+									<div class="mb-1 border-b border-solid border-gray-400">Recur</div>
+									<div>
+										{RRule.fromString(taskInfo.recur).toText()}
+									</div>
+								</div>
+							{/if}
+						</div>
+						<div class="flex">
+							{#if taskInfo.importance != 0}
+								<div class="flex-0 px-1">
+									<div class="mb-1 border-b border-solid border-gray-400">Importance</div>
+									<div>
+										{['Sub-Zero', 'Very Low', 'Low', '', 'Mid', 'High', 'Very High'][
+											taskInfo.importance + 3
+										]}
+									</div>
+								</div>
+							{/if}
+							{#if taskInfo.urgency != 0}
+								<div class="flex-0 px-1">
+									<div class="mb-1 border-b border-solid border-gray-400">Urgency</div>
+									<div>
+										{['Soon', 'Next Up', 'Why are you not doing it'][taskInfo.urgency - 1]}
+									</div>
+								</div>
+							{/if}
+							{#if taskInfo.load != 0}
+								<div class="flex-0 px-1">
+									<div class="mb-1 border-b border-solid border-gray-400">Load</div>
+									<div>
+										{['Mid', 'Hard', 'Fat Rolling'][taskInfo.load - 1]}
+									</div>
+								</div>
+							{/if}
+						</div>
+					</div>
+					<div class="flex-0">
+						<Button type="submit">{editting ? 'Update' : 'Create'}</Button>
+					</div>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
+
+<!-- <div class="flex-1">
+						
+							
 							<div class="px-4 py-2 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0" id="status-info">
 								<dt class="text-sm font-medium leading-6 text-gray-900">Status (%)</dt>
 								<dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
@@ -168,12 +256,4 @@
 								</dd>
 							</div>
 						</dl>
-					</div>
-					<div class="flex-0">
-						<Button type="submit">{editting ? 'Update' : 'Create'}</Button>
-					</div>
-				</div>
-			</form>
-		</div>
-	</div>
-</div>
+					</div> -->
