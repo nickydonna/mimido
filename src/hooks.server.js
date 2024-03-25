b3899ca235447e220a9ff86b35056da5b0de03c9
nickydonna <nicky@sudolabs.co>
2024-03-25 11:31:45 -0300

    add handle case for invalid JWT signature

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -2,6 +2,7 @@ import { getBackend } from "$lib/server/calendar";
 import { redirect } from "@sveltejs/kit";
 import jwt from 'jsonwebtoken';
 import { SESSION_KEY } from '$env/static/private';
+import { dev } from "$app/environment";
 
 const unProtectedRoutes = ['/', '/sign-in', '/sign-up'];
 
@@ -16,8 +17,21 @@ export const handle = async ({ event, resolve }) => {
     return resolve(event)
   }
 
-  const user = /** @type {App.Locals['user']} */ (jwt.verify(session, SESSION_KEY))
-  event.locals.user = user;
-  event.locals.backend = await getBackend(user);
-  return resolve(event);
+  try {
+    const user = /** @type {App.Locals['user']} */ (jwt.verify(session, SESSION_KEY))
+    event.locals.user = user;
+    event.locals.backend = await getBackend(user);
+    return resolve(event);
+  } catch (e) {
+    if (e instanceof jwt.JsonWebTokenError) {
+      event.cookies.delete('session', {
+        path: '/',
+        httpOnly: true,
+        sameSite: 'strict',
+        secure: !dev 
+      });
+      throw redirect(303, '/');
+    }
+    throw e;
+  }
 }
