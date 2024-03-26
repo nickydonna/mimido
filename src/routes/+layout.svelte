5cb3aa1a6fefd6a8071115b9349503877e5dcbee
nickydonna <nicky@sudolabs.co>
2024-03-26 10:46:24 -0300

    add copy button

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script>
 	import '../app.pcss';
 
-	import { RectangleListOutline, CalendarEditOutline, PlusOutline } from 'flowbite-svelte-icons';
+	import { RectangleListOutline, CalendarEditOutline, PlusOutline, FileCopyAltOutline } from 'flowbite-svelte-icons';
 	import BottomNav from 'flowbite-svelte/BottomNav.svelte';
 	import BottomNavItem from 'flowbite-svelte/BottomNavItem.svelte';
 	import Navbar from 'flowbite-svelte/Navbar.svelte';
@@ -11,7 +11,8 @@
 	import NavHamburger from 'flowbite-svelte/NavHamburger.svelte';
 	import { formatISO } from 'date-fns/fp';
 	import { page } from '$app/stores';
-	import { Button, Modal, Input } from 'flowbite-svelte';
+	import { copy } from 'svelte-copy';
+	import { Button, Modal, Input, Popover } from 'flowbite-svelte';
 
 	/** @type {import('./$types').LayoutData} */
 	export let data;
@@ -38,7 +39,14 @@
 					Copy this token, and use it on any other platform to copy login.
 				</p>
 				<p class="font-bold text-lg">DO NOT SHARE THIS!!!!</p>
-				<Input type="text" value={data.token} />
+				<Input type="text" value={data.token} class="select-all">
+					<button slot="right" use:copy={data.token} class="pointer-events-auto" offset="30" id="copy-token">
+						<FileCopyAltOutline />
+					</button>
+				</Input>
+				<Popover class="w-64 text-sm font-light " title="Copied" triggeredBy="#copy-token" trigger="click">
+					Paste in a new browser in the import token to login.
+				</Popover>
 				<svelte:fragment slot="footer">
 					<Button color="alternative" on:click={() => (authModal = false)}>Close</Button>
 				</svelte:fragment>
